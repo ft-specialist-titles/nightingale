d5672afed97e6c853caa50580f1bfc2eafbd008a
Luke Kavanagh <ltkavanagh@gmail.com>
2015-01-12 10:24:25 +0000

    No logo space to the right of narrow charts.

--- src/LineControls.js
+++ src/LineControls.js
@@ -15,7 +15,10 @@ var LineControls = Backbone.Model.extend({
   },
 
   overrideConfig: function(config) {
-    config.chartHeight = config.width * (3/4),
+    config.chartHeight = config.width * (3/4);
+    if (config.width < 280) {
+      config.logoSpace = false;
+    }
     config.numberAxisOrient = this.attributes.flipYAxis ? 'left' : 'right';
     config.y.zeroOrigin = config.falseorigin = !this.attributes.startFromZero;
     config.y.flip = this.attributes.flipYAxis;
