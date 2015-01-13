8d27660702492a6dae4b432c096b248f26e51cb8
Luke Kavanagh <ltkavanagh@gmail.com>
2015-01-13 16:20:58 +0000

    Guess short and long month formats when data include dates in May.

--- src/DataImport.js
+++ src/DataImport.js
@@ -701,21 +701,55 @@ var DataImport = Backbone.Model.extend({
           typeInfo.mostPopularDateFormat = formatKeys[0];
         } else if (numKeys > 1) {
 
+          var containsLongMonths;
+          var containsShortMonths;
+
           formatKeys.forEach(function (key) {
+
             var count = countDateFormats[key];
             if (count >= mostPopularDateFormatCount) {
-              // it's not good enough for the current format is be joint most-popular
+              // it's not good enough for the current format to be joint most-popular
               mostPopularDateFormat = (count === mostPopularDateFormatCount) ? null : key;
               mostPopularDateFormatCount = count;
             }
+
+            if (key.indexOf('%B') !== -1) {
+              containsLongMonths = key;
+            } else if(key.indexOf('%b') !== -1) {
+              containsShortMonths = key;
+            }
+
           });
 
-          if (mostPopularDateFormatCount &&
-                totalNonDateStrings &&
-                // the most popular date format found is more than 85%
-                // of the string values in columns (minus the values that can be parsed by the JS Date constructor)
-                (mostPopularDateFormatCount * (100 / totalNonDateStrings) > 95)) {
-            typeInfo.mostPopularDateFormat = mostPopularDateFormat;
+          function gtDateThreshold(count) {
+            return count * 100 / totalNonDateStrings > 95
+          }
+
+          if (mostPopularDateFormatCount && totalNonDateStrings) {
+            
+            // the most popular date format found is more than 95%
+            // of the string values in columns (minus the values that can be parsed by the JS Date constructor)
+            var greaterThanThreshold = gtDateThreshold(mostPopularDateFormatCount);
+
+            // handle the case when some of the dates are in the month of May
+            // we therefore have a mix of long and short month formats ie %B and %b
+            // We can gloss over this because either date format will correct parse the date on those strings
+            if (!greaterThanThreshold && containsShortMonths && containsLongMonths) {
+              if (mostPopularDateFormat === containsLongMonths) {
+                mostPopularDateFormatCount += countDateFormats[containsShortMonths.replace('%B', '%b')] || 0;
+              } else if (mostPopularDateFormat === containsShortMonths) {
+                mostPopularDateFormatCount += countDateFormats[containsShortMonths.replace('%b', '%B')] || 0;
+              }
+              // recalculate if the count is over the threshold
+              greaterThanThreshold = gtDateThreshold(mostPopularDateFormatCount);
+            }
+
+            if (greaterThanThreshold) {
+              typeInfo.mostPopularDateFormat = mostPopularDateFormat;
+            } else {
+              typeInfo.mostPopularDateFormat = null;
+            }
+
           } else {
             typeInfo.mostPopularDateFormat = null;
           }
