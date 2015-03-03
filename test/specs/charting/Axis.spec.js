359929255eb739d28d5c4f84f564eee169ceed7a
Peter Mouland <peter.mouland@gmail.com>
2015-03-03 23:46:55 +0000

    add more specs for code coverage

---
+++ test/specs/charting/Axis.spec.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = __html__['_site/index.html'];
+
+var transformNumber = require('../../../src/scripts/charting/axis');
+
+describe('transformTable module can ', function () {
+
+    it('sum an array of numbers', function () {
+
+        //expect(transformNumber(1)).toBe(1);
+
+    });
+
+});
---
+++ test/specs/transform/series.spec.js
@@ -0,0 +1,13 @@
+document.body.innerHTML = __html__['_site/index.html'];
+
+var transformNumber = require('../../../src/scripts/transform/series');
+
+describe('transformSeries module can ', function () {
+
+    it('sum an array of numbers', function () {
+
+        //expect(transformNumber(1)).toBe(1);
+
+    });
+
+});
