f8e2b4713be82d2a0e44955217214171105232f1
Peter Mouland <peter.mouland@gmail.com>
2015-03-11 16:51:01 +0000

    attempt vendor.js

--- component.config.js
+++ component.config.js
@@ -17,8 +17,17 @@ module.exports = {
             './bower_components/underscore/underscore.js',
             './bower_components/backbone/backbone.js',
             './node_modules/hbsfy/runtime.js',
-            './bower_components/d3/d3.js',
-            './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js'
+            './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+            './bower_components/d3/d3.js'
+        ],
+        vendorBundle: [
+            //{ file :'./bower_components/underscore/underscore.js', expose: 'underscore'},
+            //{ file :'./bower_components/backbone/backbone.js', expose: 'backbone'},
+            //{ file :'./node_modules/hbsfy/runtime.js', expose: 'hbsfy'},
+            //{ file :'./bower_components/d3/d3.js', expose: 'd3'},
+            //{ file :'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js', expose: 'bootstrap-sass'},
+            //{ file :'./bower_components/jquery/dist/jquery.js', expose: 'jquery'},
+            //{ file :'./node_modules/handlebars/runtime.js', expose: 'handlebars'}
         ]
     },
     test: 'karma',  // or false.  where your tests config, specs and reports are saved
