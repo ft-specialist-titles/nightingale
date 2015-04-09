b0e34fd54fa37173e8dcb650d9babd07d5dec8c0
Stuart Turner <stuart.turner@ft.com>
2015-04-09 12:09:52 +0100

    fixing condition of user signing in with non-ft.com email address - they will now be shown a notification and prompted to re-sign in with the correct account

--- src/scripts/authentication/authentication.js
+++ src/scripts/authentication/authentication.js
@@ -1,4 +1,4 @@
-/* global gapi, ng*/
+/* global gapi, ng, auth2 */
 var $ = require('jquery');
 
 var Authentication = function () {
@@ -16,13 +16,23 @@ Authentication.prototype.renderButton = function () {
 
 Authentication.prototype.onSignIn = function (googleUser) {
     var profile = googleUser.getBasicProfile();
+
     var regexp = /^.*\@ft\.com$/gi;
     if (profile.getEmail().match(regexp)){
+
         var tracking_request = "http://track.ft.com/track/track.gif?nightingale_login=" + encodeURIComponent(profile.getEmail());
-        console.log(tracking_request);
         $('#login-overlay').prepend('<img id="userlogintracking" src="'+tracking_request+'" />');
+
         $("#login-overlay").css('display', 'none');
         $("#layout").css('display', 'block');
+
+    } else {
+        //if the user has multiple google accounts then calling disconnect ensures the user will be shown the login preferences box
+        //when re-signing in (otherwise login will automatically login with their previous selection).
+        gapi.auth2.getAuthInstance().disconnect();
+        gapi.auth2.getAuthInstance().signOut();
+
+        $("#login-alert").css('display', 'block');
     }
 };
 
