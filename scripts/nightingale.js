e672df641f94f63eb69264369e12bffdeb3fecdc
circleci <{{ git.email }}>
2015-06-04 11:26:58 +0000

    v1.1.0

--- scripts/nightingale.js
+++ scripts/nightingale.js
@@ -23773,18 +23773,58 @@ function plotSeries(plotSVG, model, createdAxes, series, seriesNumber){
         .attr('height', function (d, i){ return plot.columnHeight(d.value); })
         .attr('width',  function (d, i){ return plot.columnWidth(d, i); });
 
+
+    if (!model.stack) {
+        // add N/As for null values
+        s.selectAll('text.null-label')
+            .data(data._nulls)
+            .enter()
+            .append('text')
+            .attr('class', 'null-label')
+            .attr('x',  function (d, i) { return plot.x(d.key, seriesNumber); })
+            .attr('y',  function (d, i) { return plot.y(d.value, i); })
+            .attr('dy', '-0.5em')
+            .attr('dx', function (d, i) { return plot.columnWidth(d, i) / 2;})
+            .text('n/a');
+    }
+
     styler(plotSVG);
+
+    if (!model.stack) {
+        // make those labels who don't fit smaller
+        s.selectAll('text.null-label')
+            .each(function(d, i) {
+                var w = this.getBoundingClientRect();
+                if ((w.width + 2) >= plot.columnWidth(d, i)) {
+                    this.innerHTML = '–';
+                }
+            });
+    }
 }
 
 function formatData(model, series) {
+
+    var nulls = [];
+
     var data = model.data.map(function (d){
         return{
             key:d[model.x.series.key],
             value: (Array.isArray(d.values)) ? d.values[0][series.key] : d[series.key]
         };
     }).filter(function (d) {
-        return (d.value !== null && !isNaN(d.value));
+        var isNull = !(d.value !== null && !isNaN(d.value));
+        if (isNull) nulls.push(d);
+        // if we're stacking - we transform nulls
+        // into zeros to avoid problems
+        if (model.stack && isNull) {
+            d.value = 0;
+            return true;
+        }
+        return !isNull;
     });
+
+    data._nulls = nulls;
+
     return data;
 }
 
@@ -24501,7 +24541,15 @@ function applyAttributes(g, keepD3Styles) {
             'attributes': {
                 'stroke': 'rgb(184,177,169)'
             }
+        }, {
+            'selector': '.series text.null-label',
+            'attributes': {
+                'text-anchor': 'middle',
+                'font-size': 10,
+                'fill': 'rgba(0, 0, 0, 0.4)'
+            }
         },
+
         //text
         {
             'selector': '.chart-title text, .chart-title tspan',
@@ -25450,7 +25498,7 @@ module.exports = {
 };
 
 },{}],34:[function(require,module,exports){
-module.exports = "0.3.3";
+module.exports = "0.3.4";
 },{}],35:[function(require,module,exports){
 //     Underscore.js 1.8.3
 //     http://underscorejs.org
@@ -30771,7 +30819,7 @@ function createNumberTransformer(options) {
 
         if (d === '') return _NaN;
 
-        if (d === '*') return null;
+        if (/^[*:-]$/.test(d)) return null;
 
         return Number(d);
 
