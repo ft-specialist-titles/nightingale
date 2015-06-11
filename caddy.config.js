6789b912d6e460a00ba1b8fea08333d46e04ce7f
Peter Mouland <peter.mouland@gmail.com>
2015-06-11 09:33:46 +0100

    update to use web-caddy, a faster build tool

---
+++ caddy.config.js
@@ -0,0 +1,42 @@
+var pkg = require('./package.json');
+
+module.exports = {
+    pkg: pkg,
+    paths: {
+        source: "./src",
+        "demo": false,
+        "target": './_site'
+    },
+    tasks: {
+        copy: ['images', 'fonts', 'server-config'],
+        build: ['sass', 'mustache', 'browserify'], //plus 'requirejs', 'jade'
+        test: 'karma', // or false. mocha not yet available.
+        release: ['git', 'gh-pages'],//['bower'], // ['git', 'gh-pages','s3', 'bower'] or false.
+        serve: 'staticApp', // `staticApp` or `nodeApp`
+    },
+    browserify: {
+        insertGlobals : true,
+        detectGlobals : false,
+        noParse: [
+            './bower_components/jquery/dist/jquery.js',
+            './bower_components/underscore/underscore.js',
+            './bower_components/backbone/backbone.js',
+            './node_modules/hbsfy/runtime.js',
+            './bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+            './bower_components/d3/d3.js'
+        ],
+        //vendorBundle: [
+        //{ file :'./bower_components/underscore/underscore.js', expose: 'underscore'},
+        //{ file :'./bower_components/backbone/backbone.js', expose: 'backbone'},
+        //{ file :'./node_modules/hbsfy/runtime.js', expose: 'hbsfy'},
+        //{ file :'./bower_components/d3/d3.js', expose: 'd3'},
+        //{ file :'./bower_components/bootstrap-sass/assets/javascripts/bootstrap.js', expose: 'bootstrap-sass'},
+        //{ file :'./bower_components/jquery/dist/jquery.js', expose: 'jquery'},
+        //{ file :'./node_modules/handlebars/runtime.js', expose: 'handlebars'}
+        //]
+    },
+    karma:{
+        unit: './test/karma.unit.js', // string or false. Karma config file with coverage setup.
+        unitCoverage: './test/coverage/summary.json'// code coverage summary for unit tests
+    }
+};
